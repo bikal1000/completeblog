3401ba39c0e336e3a0d9ffc86c5bc6b47e63d110
bikal1000 <bikal1000.bs@gmail.com>
2018-12-07 20:41:21 +0545

    this is first commit

---
+++ src/articles/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.ArticleListView.as_view(), name='article_list'),
+    path('<int:pk>/edit/',views.ArticleUpdateView.as_view(),name='article_edit'),
+    path('<int:pk>/',views.ArticleDetailView.as_view(),name='article_detail'),
+    path('<int:pk>/delete/',views.ArticleDeleteView.as_view(),name='article_delete'),
+    path('new/', views.ArticleCreateView.as_view(), name='article_new'),
+]
---
+++ src/articles/views.py
@@ -0,0 +1,42 @@
+from django.views.generic import ListView ,DetailView
+from django.views.generic.edit import UpdateView , DeleteView ,CreateView
+from . import models
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class ArticleListView(LoginRequiredMixin, ListView):
+    model = models.Article
+    template_name = 'article_list.html'
+    login_url = 'login'
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
+    model = models.Article
+    template_name ='article_detail.html'
+    login_url ='login'
+
+
+class ArticleUpdateView(LoginRequiredMixin, UpdateView):
+    model = models.Article
+    fields = ['title','body']
+    template_name = 'article_edit.html'
+    login_url = 'login'
+
+class ArticleDeleteView(LoginRequiredMixin, DeleteView):
+    model = models.Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+    login_url = 'login'
+
+class ArticleCreateView(LoginRequiredMixin,CreateView):
+    model = models.Article
+    template_name = 'article_new.html'
+    fields = ['title','body']
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+
+
